06af83aaeb2e5a300b5c117bcf1e31dcc0858cfc
Whichhunter404 <schmidt.peti32@gmail.com>
2019-10-16 21:02:19 +0200

    style changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-const szolgaltatas = (erdekel_btn) =>{
+function szolgaltatas(erdekel_btn){
     const data = erdekel_btn.getAttribute("data");
     const text_field = document.getElementById("bevizsgalas_type");
     text_field.value = data;
 
 };
 
-const toggle_id = (id) =>{
+function toggle_id(id){
     const toggle_div = document.getElementById(id);
     toggle_div.style.transition = "all 1s";
     if(toggle_div.style.display==="none"){
